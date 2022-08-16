76e4f89c869bdac6547c957f470e0a3d21cad09e
Cipher <kirillkuznetsov181@gmail.com>
2022-08-16 20:34:23 +0300

    add service

---
+++ src/main/java/com/cinema/project/config/Config.java
@@ -0,0 +1,16 @@
+package com.cinema.project.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "com.cinema.project")
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableScheduling
+public class Config {
+
+}
